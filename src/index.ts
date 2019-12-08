09642419ad04f89e6c06ff276a3608a1c74d9dac
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-08 15:34:57 +0200

    Colors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ const draw = canvasElement.getContext('2d') as CanvasRenderingContext2D;
 
 type Particle = {
   x: number,
-  y: number
+  y: number,
+  color: rgb
 };
 
 type Line = {
@@ -30,6 +31,8 @@ type Scene = {
 
 type Modifier = (_pointer: Pointer, _all: Particle[], inside: Particle[]) => void;
 
+const white: rgb = {r: 1, g: 1, b: 1, a: 1};
+
 function initParticles(particles: Particle[], lines: Line[], image: ImageData | null) {
   for (let y = offsetX; y < width; y += stepX) {
     for (let x = offsetY, a = null, b = null; x < height; x += stepY) {
@@ -39,7 +42,7 @@ function initParticles(particles: Particle[], lines: Line[], image: ImageData | 
         const i = (iy * image.width + ix) * 4;
 
         if (image.data[i] < 20) {
-          b = {x, y};
+          b = {x, y, color: white};
           particles.push(b);
         }
         else {
@@ -47,7 +50,7 @@ function initParticles(particles: Particle[], lines: Line[], image: ImageData | 
         }
       }
       else {
-        b = {x, y};
+        b = {x, y, color: white};
         particles.push(b);
       }
 
@@ -80,6 +83,7 @@ function drawParticles(particles: Particle[]) {
     // draw.stroke();
 
     draw.beginPath();
+    draw.fillStyle = rgb2css(particle.color);
     draw.arc(particle.x, particle.y, 1, 0, 2 * Math.PI, false);
     draw.fill();
   }
@@ -92,7 +96,9 @@ function drawLines(lines: Line[]) {
 
   for (let line of lines) {
     draw.beginPath();
+    draw.strokeStyle = rgb2css(line.a.color);
     draw.moveTo(line.a.x, line.a.y);
+    draw.strokeStyle = rgb2css(line.b.color);
     draw.lineTo(line.b.x, line.b.y);
     draw.stroke();
   }
@@ -122,6 +128,8 @@ function particlesInsidePointer(pointer: Pointer, particles: Particle[]): Partic
 }
 
 function drawPointer(pointer: Pointer) {
+  draw.strokeStyle = rgb2css(white);
+
   draw.beginPath();
   draw.arc(pointer.x, pointer.y, pointer.radius, 0, 2 * Math.PI, false);
   draw.stroke();
@@ -136,7 +144,27 @@ function drawScene(scene: Scene) {
   drawPointer(scene.pointer);
 }
 
-const modifiers: Modifier[] = [randomise, randomise2, implode, explode];
+function drawSampled(particles: Particle[], lines: Line[]) {
+  draw.fillStyle = '#333';
+  draw.fillRect(0, 0, width, height);
+
+  for (let particle of particles) {
+    for(let i = 0; i < 30; i++) {
+      const x = particle.x + (Math.random() * 20 - 10);
+      const y = particle.y + (Math.random() * 20 - 10);
+      const a = Math.random() * 0.005;
+
+      draw.beginPath();
+      draw.fillStyle = rgb2css({r: particle.color.r, g: particle.color.g, b: particle.color.b, a});
+      draw.arc(x, y, 0.1, 0, 2 * Math.PI, false);
+      draw.fill();
+    }
+  }
+
+  drawLines(lines);
+}
+
+const modifiers: Modifier[] = [colorShift, randomise, randomise2, implode, explode];
 
 function addEventListeners(scene: Scene) {
   let modifierIndex = 0;
@@ -174,7 +202,7 @@ function addEventListeners(scene: Scene) {
   canvasElement.addEventListener('wheel', (e) => {
     if (scene.pointer !== null) {
       scene.pointer.radius -= e.deltaY;
-      scene.pointer.radius = Math.min(200, Math.max(10, scene.pointer.radius));
+      scene.pointer.radius = Math.min(500, Math.max(10, scene.pointer.radius));
 
       drawScene(scene);
     }
@@ -193,6 +221,9 @@ function addEventListeners(scene: Scene) {
   document.addEventListener('keydown', (e) => {
     if (e.keyCode === 70) {
       modifierIndex = (modifierIndex + 1) % modifiers.length;
+    }
+    else if (e.keyCode === 83) {
+      drawSampled(scene.particles, scene.lines);
     }
   });
 }
@@ -228,7 +259,7 @@ function go(image: ImageData | null) {
     radius: 100
   };
 
-  const scene: Scene = { particles, lines, pointer };
+  const scene: Scene = {particles, lines, pointer};
 
   // for debugging
   (window as any).debugScene = scene;
@@ -238,7 +269,8 @@ function go(image: ImageData | null) {
   drawScene(scene);
 }
 
-loadImage('http://localhost:8003/image.png', go);
+// loadImage('http://localhost:8003/image2.png', go);
+go(null);
 
 // -----------------------------------------------------------------------------
 
@@ -276,4 +308,112 @@ function explode(pointer: Pointer, all: Particle[], inside: Particle[]): void {
     particle.x = particle.x + dx / ((d * d) / pointer.radius);
     particle.y = particle.y + dy / ((d * d) / pointer.radius);
   }
+}
+
+function colorShift(pointer: Pointer, all: Particle[], inside: Particle[]): void {
+  for (let particle of inside) {
+    const dx = particle.x - pointer.x;
+    const dy = particle.y - pointer.y;
+    const d = Math.max(1, Math.sqrt(dx * dx + dy * dy));
+
+    // const hue = 360.0 * ((d < pointer.radius / 2) ? 0.5 : 0.833);
+    let h;
+    if (d < (pointer.radius / 2 - 20)) {
+      h = 0.5;
+    }
+    else if (d > (pointer.radius / 2 + 20)) {
+      h = 0.833;
+    }
+    else {
+      h = lerp(0.5, 0.833, (d - (pointer.radius / 2 - 20)) / 40.0);
+    }
+
+    const hue = 360.0 * lerp(0.5, 0.833, d / pointer.radius);
+    const sat = d / (pointer.radius);
+
+    particle.color = hsv2rgb({h: h * 360.0, s: sat, v: 1});
+  }
+}
+
+function lerp(a: number, b: number, t: number): number {
+  return a + (b - a) * t;
+}
+
+function rad2deg(r: number): number {
+  return r * 180.0 / Math.PI;
+}
+
+type hsv = {
+  h: number,
+  s: number,
+  v: number
+};
+
+type rgb = {
+  r: number,
+  g: number,
+  b : number,
+  a: number
+};
+
+function hsv2rgb(hsv: hsv): rgb {
+  let hh, p, q, t, ff;
+  let i;
+  let out: rgb = {r: 0, g: 0, b: 0, a: 1};
+
+  if(hsv.s <= 0.0) {       // < is bogus, just shuts up warnings
+    out.r = hsv.v;
+    out.g = hsv.v;
+    out.b = hsv.v;
+    return out;
+  }
+  hh = hsv.h;
+  if(hh >= 360.0) hh = 0.0;
+  hh /= 60.0;
+  i = Math.floor(hh);
+  ff = hh - i;
+  p = hsv.v * (1.0 - hsv.s);
+  q = hsv.v * (1.0 - (hsv.s * ff));
+  t = hsv.v * (1.0 - (hsv.s * (1.0 - ff)));
+
+  switch(i) {
+    case 0:
+      out.r = hsv.v;
+      out.g = t;
+      out.b = p;
+      break;
+    case 1:
+      out.r = q;
+      out.g = hsv.v;
+      out.b = p;
+      break;
+    case 2:
+      out.r = p;
+      out.g = hsv.v;
+      out.b = t;
+      break;
+
+    case 3:
+      out.r = p;
+      out.g = q;
+      out.b = hsv.v;
+      break;
+    case 4:
+      out.r = t;
+      out.g = p;
+      out.b = hsv.v;
+      break;
+    case 5:
+    default:
+      out.r = hsv.v;
+      out.g = p;
+      out.b = q;
+      break;
+  }
+
+  return out;
+}
+
+function rgb2css(rgb: rgb): string {
+  return 'rgba(' + (rgb.r * 255.0) + ', ' + (rgb.g * 255.0) + ', ' + (rgb.b * 255.0) + ', ' + (rgb.a * 255.0) + ')';
 }
